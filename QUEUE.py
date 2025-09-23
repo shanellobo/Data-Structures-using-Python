3d7c84a2034e01ec0ba2c0a8feaeba1b5f490b9c
shanellobo <shanelllobo06@gmail.com>
2025-09-23 13:37:08 +0400

    QUEUE.py

--- QUEUE.py
+++ QUEUE.py
@@ -4,28 +4,30 @@ class Queue:
     
     def enqueue(self, item):
         self.queue.append(item)
-        print(f"Enqueued:{item}")
+        print(f"Enqueued: {item}")
     
     def is_empty(self):
-        if len(self.queue)==0:
-            print("Queue is empty")
-            
+        return len(self.queue) == 0  # Corrected to return a boolean value
+    
     def size(self):
-         print("Size of queue is",len(self.queue))
+        print("Size of queue is", len(self.queue))
     
     def dequeue(self):
         if self.is_empty():
             return "Queue is empty"
-        print("Removed",self.queue.pop(0))
+        print("Removed", self.queue.pop(0))
     
     def display(self):
-        print("Queue",self.queue)
+        print("Queue", self.queue)
         
-q= Queue()        
-n=int(input("Enter the number of elements in Queue: "))
+q = Queue()        
+n = int(input("Enter the number of elements in Queue: "))
 for i in range(n):
-    q.enqueue(int(input(f"Enter element {i+1}: ")))
+    q.enqueue(int(input(f"Enter element {i + 1}: ")))
+    
 q.dequeue()
 q.size()
+print(q.is_empty())
 q.display()
+
     
